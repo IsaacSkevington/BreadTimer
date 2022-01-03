f16478a2f488368240d0e207cb59113bb0ad5ead
IsaacSkevington <56514428+IsaacSkevington@users.noreply.github.com>
2022-01-03 11:46:01 +0000

    Added clementine cake

---
+++ recipes/Clementine Cake.js
@@ -0,0 +1,128 @@
+var r = new Recipe(
+
+    'Clementine Cake',
+    'No-flour clementine cake',
+    [
+        new Step(
+            1,
+            'Put the clementines in a pan with some cold water, bring to the boil and cook for 2 hours.',
+            [
+                new Ingredient(
+                    'Clementines',
+                    '375',
+                    'g'),
+
+            ],
+            7200),
+
+        new Step(
+            2,
+            'Drain and, when cool, cut each clementine in half and remove the pips.',
+            [
+
+            ],
+            300),
+
+        new Step(
+            3,
+            'Pulp everything - skin, pith, fruit - in the processor.',
+            [
+
+            ],
+            300),
+
+        new Step(
+            4,
+            'Preheat the oven to gas mark 5 / 190oC.',
+            [
+
+            ],
+            60),
+
+        new Step(
+            5,
+            'Butter and line a 21cm Springform tin.',
+            [
+
+            ],
+            300),
+
+        new Step(
+            6,
+            'Beat the eggs.',
+            [
+                new Ingredient(
+                    'eggs',
+                    '6',
+                    'g'),
+
+            ],
+            180),
+
+        new Step(
+            7,
+            'Add the sugar, almonds and baking powder and mix well.',
+            [
+                new Ingredient(
+                    'sugar',
+                    '225',
+                    'g'),
+                new Ingredient(
+                    'ground almonds',
+                    '250',
+                    'g'),
+                new Ingredient(
+                    'baking powder',
+                    '1.25',
+                    'tsp'),
+
+            ],
+            300),
+
+        new Step(
+            8,
+            'Add pulped oranges and stir lightly.',
+            [
+
+            ],
+            300),
+
+        new Step(
+            9,
+            'Pour the cake mixture into the prepared tin.',
+            [
+
+            ],
+            180),
+
+        new Step(
+            10,
+            'Bake for 40 minutes.',
+            [
+
+            ],
+            2400),
+
+        new Step(
+            11,
+            'Cover with foil or greaseproof and bake for a further 20 minutes (or until a skewer comes out clean).',
+            [
+
+            ],
+            1320),
+
+        new Step(
+            12,
+            'Remove from the oven ands leave to cool on a rack, but in the tin.',
+            [
+
+            ],
+            3600),
+
+    ],
+
+    {},//Groups
+
+    {}//Repeats
+);
+RECIPEBOOK.add(r);
